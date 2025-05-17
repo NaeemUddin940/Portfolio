498862ba9a72da190bcd4e239628b16b1277fe17
NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-05-17 08:33:02 +0600

    show-contact page route setup

--- Contact-app/controller/.controller.js
+++ Contact-app/controller/.controller.js
@@ -1,3 +1,11 @@
+
+// Home Page Get Route
 export const homePage = (req, res) => {
   res.render("home");
+};
+
+
+// Show Contact get route
+export const showContact = (req, res) => {
+  res.render("show-contact");
 };
--- Contact-app/routes/.routes.js
+++ Contact-app/routes/.routes.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { homePage } from "../controller/.controller.js";
+import { homePage, showContact } from "../controller/.controller.js";
 const router = express.Router();
 
 router.get("/", homePage);
+router.get("/show-contact", showContact);
+// router.get("/", homePage);
 
 export default router;
